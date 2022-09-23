e734c1a6bce67421aad41d6a009d8573bdf0a850
zxl5683995 <114218738+zxl5683995@users.noreply.github.com>
2022-09-23 16:14:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@
 # 模板消息id
 "template_id": "w73-Db6BYNcGwt5glGdfDJFjoNcLF3kcOwpUj7HAQGQ",
 # 接收公众号消息的微信号id，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-"user": ["o1hEg5sQqtPF_KsWNP5BkCYl0WwU","o1hEg5mQ6qE2T217UAQkpcSGsW68"],
+"user": ["o1hEg5mQ6qE2T217UAQkpcSGsW68"],
 
 # 信息配置
 # 所在省份
