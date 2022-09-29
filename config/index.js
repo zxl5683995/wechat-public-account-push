2b822348c3079573cbba7a28f7341d56224f0f76
zxl5683995 <114218738+zxl5683995@users.noreply.github.com>
2022-09-29 10:01:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,14 +4,7 @@ export const USER_CONFIG = {
    */
   
   // 公众号APP_ID
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx2afb4f9af71602af",
-  
-  // 公众号APP_SECRET
-  //
-  
-    holidaytts: true,
+  // 建议不要填这里，请使用文档中github secret的方法
     
     /** 每日N句 */
     // 金山每日一句
